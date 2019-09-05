31d39feb04fd6eefcdded85d936d45cd31ae152c
Cam McLeod <cammcleod3@gmail.com>
2019-09-05 14:41:58 -0700

    added default state without request

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,6 +2,11 @@ import Vapor
 
 /// Register your application's routes here
 public func routes(_ router: Router) throws {
+    
+    // Basic "It works" example
+    router.get { req in
+        return "It works!"
+    }
 
     /// return a String
     router.get("name") { req in
@@ -20,6 +25,11 @@ public func routes(_ router: Router) throws {
     
     router.get("view") { req -> Future<View> in
         return try req.view().render("welcome")
+    }
+    
+    router.get("bonus") { req -> Future<View> in
+        let developer = Person(name: "Martin", age: 26)
+        return try req.view().render("whoami", developer)
     }
 }
 
